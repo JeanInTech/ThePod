5f7bae3c93776e7d19f8cbc94d59ad0665f3bc14
Jean Thomas <jeniferjeanthomas@gmail.com>
2020-12-03 17:38:30 -0500

    Included new project file and planning folder

---
+++ ThePod/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace ThePod.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
