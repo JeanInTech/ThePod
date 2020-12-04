591a24ba66b54c43929aa5d624912b38b91d91fa
duncaannn <drbigfupa@yahoo.com>
2020-12-04 14:50:05 -0500

    Working on printing from API

--- ThePod/Controllers/PodController.cs
+++ ThePod/Controllers/PodController.cs
@@ -1,4 +1,5 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Configuration;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -8,7 +9,7 @@ using ThePod.Models;
 
 namespace ThePod.Controllers
 {
-    public class PodController
+    public class PodController:Controller
     {
         private readonly PodDAL _dal;
         private readonly IConfiguration _config;
@@ -17,9 +18,14 @@ namespace ThePod.Controllers
             _dal = dal;
             _config = config;
         }
-        //public async Task<Shows> GetShowsAsync()
-        //{
-        //    return ErrorViewModel();
-        //}
+      
+        public async Task<IActionResult> Index(string id)
+        {
+            var result = await _dal.GetEpisodeAsync(id);
+
+            return View(result);
+        }
+
+
     }
 }
--- ThePod/DataAccess/PodDAL.cs
+++ ThePod/DataAccess/PodDAL.cs
@@ -2,9 +2,11 @@
 using System.Collections.Generic;
 using System.Linq;
 using System.Net.Http;
+
 using System.Text.Json;
 using System.Threading.Tasks;
 using ThePod.Models;
+
 
 namespace ThePod.DataAccess
 {
@@ -23,6 +25,18 @@ namespace ThePod.DataAccess
             Shows s = JsonSerializer.Deserialize<Shows>(jsonData);
 
             return s;
+        }
+
+
+        public async Task<Show> GetEpisodeAsync(string id)
+        {
+            var response = await _client.GetAsync($"search?id={id}");
+            var jsonData = await response.Content.ReadAsStringAsync();
+            Show shows = JsonSerializer.Deserialize<Show>(jsonData);
+
+
+            
+            return shows;
         }
     }
 }
