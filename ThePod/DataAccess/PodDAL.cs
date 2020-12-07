75e2948f60981e600a802b8ea8dc7856108ff5b0
Kristen Harrell <71024265+kristen-harrell@users.noreply.github.com>
2020-12-07 15:58:39 -0500

    added addtl contoller methods for searching

--- ThePod/DataAccess/PodDAL.cs
+++ ThePod/DataAccess/PodDAL.cs
@@ -62,5 +62,36 @@ namespace ThePod.DataAccess
 
             return ro;
         }
+        public async Task<RootobjectEpisodes> SearchEpisodeNameAsync(string query)
+        {
+            var token = await GetToken();
+            HttpClient client = new HttpClient();
+            //var encodedQuery = Uri.EscapeDataString(query);
+
+            client.BaseAddress = new Uri("https://api.spotify.com/v1/");
+            client.DefaultRequestHeaders.Add("Authorization", $"{token.token_type} {token.access_token}");
+            var response = await client.GetAsync($"search?q={query}&type=episode&market=US");
+            var jsonData = await response.Content.ReadAsStringAsync();
+
+            RootobjectEpisodes roe = JsonSerializer.Deserialize<RootobjectEpisodes>(jsonData);
+
+            return roe;
+        }
+        public async Task<RootEpisodes> SearchEpisodeIdAsync(string query)
+        {
+            var token = await GetToken();
+            HttpClient client = new HttpClient();
+            //var encodedQuery = Uri.EscapeDataString(query);
+
+            client.BaseAddress = new Uri("https://api.spotify.com/v1/");
+            client.DefaultRequestHeaders.Add("Authorization", $"{token.token_type} {token.access_token}");
+            var response = await client.GetAsync($"episodes?id={query}&market=US");
+            var jsonData = await response.Content.ReadAsStringAsync();
+
+            RootEpisodes roe = JsonSerializer.Deserialize<RootEpisodes>(jsonData);
+
+            return roe;
+        }
+
     }
 }
---
+++ ThePod/Models/Episodes.cs
@@ -0,0 +1,64 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ThePod.Models
+{
+
+    public class RootobjectEpisodes
+    {
+        public Episodes episodes { get; set; }
+    }
+
+    public class Episodes
+    {
+        public string href { get; set; }
+        public EpisodeItem[] items { get; set; }
+        public int limit { get; set; }
+        public string next { get; set; }
+        public int offset { get; set; }
+        public object previous { get; set; }
+        public int total { get; set; }
+    }
+
+    public class EpisodeItem
+    {
+        public string audio_preview_url { get; set; }
+        public string description { get; set; }
+        public int duration_ms { get; set; }
+        public bool _explicit { get; set; }
+        public External_Urls external_urls { get; set; }
+        public string href { get; set; }
+        public string id { get; set; }
+        public Image[] images { get; set; }
+        public bool is_externally_hosted { get; set; }
+        public bool is_playable { get; set; }
+        public string language { get; set; }
+        public string[] languages { get; set; }
+        public string name { get; set; }
+        public string release_date { get; set; }
+        public string release_date_precision { get; set; }
+        public string type { get; set; }
+        public string uri { get; set; }
+        public Restriction restriction { get; set; }
+    }
+
+    public class Episode_External_Urls
+    {
+        public string spotify { get; set; }
+    }
+
+    public class Restriction
+    {
+        public string reason { get; set; }
+    }
+
+    public class EpisodeImage
+    {
+        public int height { get; set; }
+        public string url { get; set; }
+        public int width { get; set; }
+    }
+
+}
