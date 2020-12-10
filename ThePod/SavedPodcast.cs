40f71dc26062bd2eefb927417dcdd571efef2d06
pinvue91 <pin.vue@hotmail.com>
2020-12-09 22:53:34 -0500

    scaffolded db

--- ThePod/SavedPodcast.cs
+++ ThePod/SavedPodcast.cs
@@ -3,23 +3,23 @@ using System.Collections.Generic;
 
 #nullable disable
 
-namespace ThePod.Models
+namespace ThePod
 {
-    public partial class UserFeedback
+    public partial class SavedPodcast
     {
         public int Id { get; set; }
         public string UserId { get; set; }
         public string EpisodeId { get; set; }
-        public string Tags { get; set; }
-        public byte? Rating { get; set; }
-        public string Review { get; set; }
-        public string EpisodeName { get; set; }
         public string PodcastName { get; set; }
+        public string EpisodeName { get; set; }
+        public string Publisher { get; set; }
         public string Description { get; set; }
         public string AudioPreviewUrl { get; set; }
-        public string ImageUrl { get; set; }
-        public DateTime? ReleaseDate { get; set; }
         public string ExternalUrls { get; set; }
+        public string ImageUrl { get; set; }
+        public int? Duration { get; set; }
+        public DateTime? ReleaseDate { get; set; }
+
         public virtual AspNetUser User { get; set; }
     }
 }
