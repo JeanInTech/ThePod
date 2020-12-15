974efcef77c711837e9f1f5d8ad2a7065b3df54f
duncaannn <drbigfupa@yahoo.com>
2020-12-15 15:00:40 -0500

    Added carousel to Grey Search box, fixed home/index so that it isnt based on a new user's lack of tags, which was throwing exceptions

--- ThePod/Controllers/HomeController.cs
+++ ThePod/Controllers/HomeController.cs
@@ -20,11 +20,7 @@ namespace ThePod.Controllers
             _dal = dal;
             _context = context;
         }
-
-        //public IActionResult Index()
-        //{
-        //    return View();
-        //}
+      
         public IActionResult Recommendations()
         {
             return View(_context.UserFeedbacks.ToList());
@@ -48,67 +44,23 @@ namespace ThePod.Controllers
 
         public async Task<IActionResult> Index()
         {
-            List<string> usersTopTags = GetProfile(); //get a list of the users top tags (the tag they used most frequently on episodes rated 3+)
-            if (usersTopTags.Count < 3)
-            {
-                return View("userrecommendations");
-            }
-            string firstPreferred = usersTopTags[0]; //1st place tag (this is just the single word(tag) so this can be used later in a viewbag
-            string secondPreferred = usersTopTags[1]; //2nd place tag
-            string thirdPreferred = usersTopTags[2]; //3rd place tag
-            ViewBag.FirstTag = firstPreferred;
-            ViewBag.SecondTag = " - " + secondPreferred;
-            ViewBag.ThirdTag = " - " + thirdPreferred;
+
 
             List<UserProfile> bestProfiles = GetBestEpisodesRawData(); //list of every tag in UserProfile table with a rating of 3+, that the logged in user has not reviewed, organized by highest rated first
 
-            List<string> firstTagEpisodeRec = new List<string>();
-            List<string> secondTagEpisodeRec = new List<string>();
-            List<string> thirdTagEpisodeRec = new List<string>();
 
-            foreach (UserProfile u in bestProfiles)
-            {
-                if (firstPreferred == u.Tag)
-                {
-                    firstTagEpisodeRec.Add(u.EpisodeId);
-                }
-                if (secondPreferred == u.Tag)
-                {
-                    secondTagEpisodeRec.Add(u.EpisodeId);
-                }
-                if (thirdPreferred == u.Tag)
-                {
-                    thirdTagEpisodeRec.Add(u.EpisodeId);
-                }
-            }
-            List<List<string>> topThreeEpisdodeLists = new List<List<string>>(); //these are just lists of strings (episode Ids)
-            {
-                topThreeEpisdodeLists.Add(firstTagEpisodeRec);
-                topThreeEpisdodeLists.Add(secondTagEpisodeRec);
-                topThreeEpisdodeLists.Add(thirdTagEpisodeRec);
-            }
+
             List<string> episodeIds = new List<string>();
-            foreach (var e in firstTagEpisodeRec)
+           
+            foreach (UserProfile e in bestProfiles)
             {
-                if (e != null)
+                if (e != null && !episodeIds.Contains(e.EpisodeId))
                 {
-                    episodeIds.Add(e);
+                    episodeIds.Add(e.EpisodeId);
                 }
+              
             }
-            foreach (var e in secondTagEpisodeRec)
-            {
-                if (e != null)
-                {
-                    episodeIds.Add(e);
-                }
-            }
-            foreach (var e in thirdTagEpisodeRec)
-            {
-                if (e != null)
-                {
-                    episodeIds.Add(e);
-                }
-            }
+          
             var epId = String.Join(",", episodeIds);
 
             var recommendedEpisodes = await _dal.SearchEpisodeIdAsync(epId);
